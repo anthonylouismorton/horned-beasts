65960c6cfde5206f1c3d8856459a34f2d926d7b6
Anthony Louis Morton <anthonymorton760@gmail.com>
2021-09-14 16:59:48 -0700

    fixed day 1 lab

--- horned-beasts/src/App.js
+++ horned-beasts/src/App.js
@@ -1,35 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { Component } from 'react';
 import Header from './Header.js';
+import Footer from './Footer.js';
+import Main from './Main.js';
 function App() {
   return (
     <div>
       <Header />
-      <Main message={'Hi there'}/>
+      <Main />
       <Footer />
     </div>
   );
 }
 
-class Footer extends Component{
-  render() {
-    return <h1>I am a footer</h1>
-  }
-}
-class Main extends Component{
-  render() {
-    return (
-    <>
-      <h1>I am a Main. My message is {this.props.message}
-      </h1>
-      <HornedBeast title={'Rhino'}/>
-    </>
-    )
-  }
-}
-class HornedBeast extends Component {
-  render() {
-    return <h1> I am a horned beast with title of {this.props.title}</h1>
-  }
-}
 export default App;
---
+++ horned-beasts/src/Footer.js
@@ -0,0 +1,7 @@
+import { Component } from 'react';
+class Footer extends Component{
+  render() {
+    return <h1>Created by Anthony Morton</h1>
+  }
+}
+export default Footer;
